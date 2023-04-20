d63ead9819d2ccd0519bf7bdb809b50d7aeea777
FinnRG <finngaertner@protonmail.com>
2023-04-20 17:57:37 +0200

    feat: Challenges Accordion view

--- app/src/Challenges.tsx
+++ app/src/Challenges.tsx
@@ -1,16 +1,43 @@
-import { Stack } from "@mantine/core";
+import { Accordion, Container, Grid, Stack } from "@mantine/core";
 import colorMap from "./utils/colorMap";
 import challenges from "./challenges.json";
 import ChallengeCard from "./components/ChallengeCard";
+import { useListState } from "@mantine/hooks";
+import { Challenge } from "./types";
+
+const getCategories = (challenges: Challenge[]) => {
+  const l = [...new Set(challenges.flatMap((ch) => ch.category))];
+  return l;
+};
+
 
 const Homescreen = (): JSX.Element => {
-  const map = colorMap(challenges);
 
-  const challengeCards = challenges.map((challenge, key) => (
-    <ChallengeCard challenge={challenge} colorMap={map} key={key} />
-  ));
+  const [value, setValueHandlers] = useListState<string>([])
+  const colors = colorMap(challenges);
+  const categories = getCategories(challenges)
+  
+  console.log(categories)
 
-  return <Stack align="center">{challengeCards}</Stack>;
+  return <Accordion  multiple value={value} onChange={setValueHandlers.setState}>
+    {categories.map((category, key) => (
+      <Accordion.Item value={category} key={key}>
+        <Accordion.Control>{category.toUpperCase()}</Accordion.Control>
+        <Accordion.Panel>
+          <Grid>
+            {challenges
+              .filter(ch => ch.category === category)
+              .map((ch, key) => (
+                <Grid.Col span={6} key={key}>
+                  <ChallengeCard challenge={ch} colorMap={colors} />
+                </Grid.Col>
+              ))}
+          </Grid>
+        </Accordion.Panel>
+      </Accordion.Item>
+    ))}
+  </Accordion>
+ ;
 };
 
 export default Homescreen;
--- app/src/components/ChallengeCard.tsx
+++ app/src/components/ChallengeCard.tsx
@@ -1,4 +1,4 @@
-import { Badge, Card, Group, Text } from "@mantine/core";
+import { Badge, Card, Group, Spoiler, Text } from "@mantine/core";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { Challenge } from "../types";
@@ -25,7 +25,17 @@ const ChallengeCard = ({ challenge, colorMap }: ChallengeCardProps) => {
       <Text size="lg" weight={500} mt="md">
         {t(`challenge-${challenge.index}-title`)}
       </Text>
-      <Text size="sm" color="dimmed" mt={5}>
+      <Text
+        style={{
+          textOverflow: "ellipsis",
+          overflow: "hidden",
+          display: "-webkit-box",
+          WebkitBoxOrient: "vertical",
+          WebkitLineClamp: 5,
+        }}
+        color="dimmed"
+        size="sm"
+      >
         {t(`challenge-${challenge.index}-content`)}
       </Text>
     </Card>
